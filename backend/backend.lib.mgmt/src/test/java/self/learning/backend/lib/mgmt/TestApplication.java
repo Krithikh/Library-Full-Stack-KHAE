50b687ae50d112fe166f426bdd3634b8fde09d05
SMARTCLIFF\1422 <asd@gmail.com>
2026-08-15 20:11:58 +0530

    Intial Commit of the EMpty Folders

---
+++ backend/backend.lib.mgmt/src/test/java/self/learning/backend/lib/mgmt/TestApplication.java
@@ -0,0 +1,11 @@
+package self.learning.backend.lib.mgmt;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
